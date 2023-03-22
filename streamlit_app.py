c0efb6874ebda99afa4107ce7e0c3805285f3f24
Daniel Timmers <37965754+danieltimmers@users.noreply.github.com>
2023-03-22 19:06:12 -0400

    Update streamlit_app.py
    removed display of raw json data

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # Displays json data
 
 # Normalize the json data from the api call 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
