226830b1b6e9ae9ef800a5b1589c4fc8518d082f
Daniel Timmers <37965754+danieltimmers@users.noreply.github.com>
2023-03-22 19:04:16 -0400

    Update streamlit_app.py
    added a table to display data we received from api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) # Displays json data
+
+# Normalize the json data from the api call 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Display a table of the normalized data 
+streamlit.dataframe(fruityvice_normalized)
