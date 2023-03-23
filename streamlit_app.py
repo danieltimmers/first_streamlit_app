9378f18fabb255f93f05b05f42be651b4cc603e8
Daniel Timmers <37965754+danieltimmers@users.noreply.github.com>
2023-03-23 13:10:17 -0400

    Update streamlit_app.py
    made the api call and response a function

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#function that accepts user text input(a fruit name) and performs an api call to fruityvice.com and returns normalized json data
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 # New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -33,13 +39,13 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_funtion)
     
 except URLError as e:
   streamlit.error()
 
+  
 # Don't run anything past here while we troubleshoot
 streamlit.stop()
 
